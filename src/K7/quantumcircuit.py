3c81ef6277576c6ba1ef1030e66de74dc36fdc6a
Brandon Jia <bljia2020@gmail.com>
2026-07-23 17:22:36 -0400

    game verifier, initial quantum circuit

---
+++ src/K7/quantumcircuit.py
@@ -0,0 +1,58 @@
+import numpy as np
+import qiskit
+from qiskit import QuantumCircuit
+from qiskit import UnitaryGate
+#not needed in vs code but needed here
+from src.bases import bases
+
+
+#create the 7 8x8 matrices
+def matrix(m):
+  eight_m = np.identity(8, dtype=complex)
+  eight_m[:6, :6] = m
+  return eight_m 
+
+bases_eight = [matrix(m) for m in bases]
+
+
+
+
+def alicestrategy(vertice):
+  matrix = bases_eight[vertice-1]
+  gate = UnitaryGate(matrix)
+  return gate
+
+def bobstrategy(vertice):
+  matrix = bases_eight[vertice-1]
+  gate = UnitaryGate(matrix)
+  return gate
+
+#the actual circuit based on two inputs
+def game_circuit(a, b):
+  #create the entangled state: Alice 3 qubits, Bob 3 qubits.
+  #We don't use |7> or |8>. we first initialize the first three qubits
+  circuit = QuantumCircuit(6)
+  circuit.initialize([1,1,1,1,1,1,0,0], [0,1,2], normalize = True)
+
+  #bob's state should mirror alice's state
+  circuit.cx(0,3)
+  circuit.cx(1,4)
+  circuit.cx(2,5)
+
+  #000,000+001,001+010,010+011,011+100,100+101,101
+  
+  #find the gates we use
+  agate = alicestrategy(a)
+  bgate = bobstrategy(b)
+  
+  #apply the gates
+  circuit.append(agate, [0,1,2])
+
+  circuit.append(bgate, [3,4,5])
+
+  circuit.measure_all()
+  return circuit
+
+
+
+
---
+++ src/K7/verifier.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+#all 21 edges
+edges = []
+
+for i in range(1,8):
+  for j in range(i+1, 8):
+    edges.append((i,j))
+
+#given a vertex i, gives all edges connected to that vertex i
+vertex_edges = []
+for i in range(1,8):
+  vertex_edges[i] = []
+
+  for edge in edges:
+    if i in edge:
+      vertex_edges[i].append[edge]
+
+def game_result(a,b):
+  #if they return the same edge, they won
+  if a == b:
+    return 1
+  #if they return disjoint edges, they won (this can't happen if Alice and Bob are given the same vertex)
+  if a[0] != b[0] and a[0] != b[1]:
+    if a[1] != b[0] and 1[1] != b[1]:
+      return 1
+
+  return 0
