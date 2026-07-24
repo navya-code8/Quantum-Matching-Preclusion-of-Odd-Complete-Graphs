5571053f754c65dd04c4277b9488913daa8de8b9
malhotrarnav09 <malhotrarnav02@gmail.com>
2026-07-24 12:37:28 -0500

    Update K7 quantum circuit

--- src/K7/quantumcircuit.py
+++ src/K7/quantumcircuit.py
@@ -17,8 +17,6 @@ def matrix(m):
 bases_eight = [matrix(m) for m in bases]
 
 
-
-
 def alicestrategy(vertice):
   matrix = bases_eight[vertice-1]
   gate = UnitaryGate(matrix)
@@ -26,7 +24,7 @@ def alicestrategy(vertice):
 
 def bobstrategy(vertice):
   matrix = bases_eight[vertice-1]
-  gate = UnitaryGate(matrix)
+  gate = UnitaryGate(np.conjugate(matrix))
   return gate
 
 #the actual circuit based on two inputs
@@ -61,8 +59,8 @@ def game_circuit(a, b):
 #get the measurements as a string
   string = list(result.get_counts())[0]
 #separate the first three and last three elements in the string above
-  alicestring = string[:3]
-  bobstring = string[3:]
+  bobstring = string[:3]
+  alicestring = string[3:]
 #convert the string from base 2 to base 10 
   aliceindex = int(alicestring[0])*4+int(alicestring[1])*2+int(alicestring[2])
   bobindex = int(bobstring[0])*4+int(bobstring[1])*2 + int(bobstring[2])
@@ -74,10 +72,12 @@ def game_circuit(a, b):
 
 
 #testing the algorithm
-alice_vertex = int(input("Enter Alice's vertex: "))
-bob_vertex = int(input("Enter Bob's vertex: "))
 
-alice_edge, bob_edge = game_circuit(alice_vertex, bob_vertex)
-print("Alice's edge:", alice_edge)
-print("Bob's edge:", bob_edge)
-print("Game result:", game_result(alice_edge, bob_edge))
+for alice_vertex in range(1, 8):
+  for bob_vertex in range(1, 8):   
+    print("Alice's vertex:", alice_vertex)
+    print("Bob's vertex:", bob_vertex)
+    alice_edge, bob_edge = game_circuit(alice_vertex, bob_vertex)
+    print("Alice's edge:", alice_edge)
+    print("Bob's edge:", bob_edge)
+    print("Game result:", game_result(alice_edge, bob_edge))
